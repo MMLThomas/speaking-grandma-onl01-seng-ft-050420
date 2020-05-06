23e18186c579e5dc0f31af19ed38ffbdbd4c5d9a
michael thomas <michael.thomas2014@gmail.com>
2020-05-06 05:57:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
+
 def speak_to_grandma(arg)
-  if arg != arg.upcase
+  if arg !== arg.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif arg == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
